24bbd0637c912d25491925f644e5893d3f7c1cab
Eren Karaboğa <darkbull1073@gmail.com>
2022-05-28 14:40:02 +0300

    Help & RateApp Added

---
+++ lib/views/devices/lightPage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class LightPage extends StatefulWidget {
+  const LightPage({Key? key}) : super(key: key);
+
+  @override
+  _LightPageState createState() => _LightPageState();
+}
+
+class _LightPageState extends State<LightPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
