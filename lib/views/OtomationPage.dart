d8c9b7612cd51f240f3f734117c5ef615963d189
Eren Karaboğa <darkbull1073@gmail.com>
2022-05-28 14:39:56 +0300

    Help & RateApp Added

--- lib/views/OtomationPage.dart
+++ lib/views/OtomationPage.dart
@@ -4,6 +4,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:avatar_glow/avatar_glow.dart';
+
+import 'devices/lightPage.dart';
 class OtomationPage extends StatefulWidget {
   const OtomationPage({Key? key}) : super(key: key);
 
@@ -28,15 +30,14 @@ class _OtomationPageState extends State<OtomationPage> {
       ),
       body: Column(
         children: [
-          buildListTile(Colors.green,Colors.green,'assets/icons/drill.png',"SCOREN"),
-          buildListTile(Colors.green,Colors.green,'assets/icons/lamb.png',"I-Light"),
-          buildListTile(Colors.red,Colors.red,'assets/icons/driller.png',"ShaveArt"),
+          buildListTileScoren(Colors.green,Colors.green,'assets/icons/drill.png',"SCOREN"),
+          buildListTileLight(Colors.green,Colors.green,'assets/icons/lamb.png',"I-Light"),
+          buildListTileShave(Colors.red,Colors.red,'assets/icons/driller.png',"ShaveArt"),
         ],
       ),
     );
   }
-/*'assets/icons/drill.png'*/
-  Padding buildListTile(Color glowColor,Color iconColor,String iconPath,String text) {
+  Padding buildListTileScoren(Color glowColor,Color iconColor,String iconPath,String text) {
     return Padding(
           padding: const EdgeInsets.all(20.0),
           child: Card(
@@ -61,5 +62,60 @@ class _OtomationPageState extends State<OtomationPage> {
           ),
         ),
     );
+
+  }
+  Padding buildListTileLight(Color glowColor,Color iconColor,String iconPath,String text) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Card(
+        elevation: 20,
+        child: ListTile(
+          onTap: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>LightPage()));
+          },
+          trailing: AvatarGlow(
+            glowColor: glowColor,
+            endRadius: 20.0,
+            duration: Duration(milliseconds: 1000),
+            repeat: true,
+            showTwoGlows: true,
+            repeatPauseDuration: Duration(milliseconds: 300),
+            child: Icon(CupertinoIcons.antenna_radiowaves_left_right,color: iconColor,size: 30),
+          ),
+          contentPadding: EdgeInsets.fromLTRB(10,20,20,20,),
+          leading: Image.asset(iconPath),
+          title:Text(text,style: TextStyle(color: Colors.black54,fontSize: 20,fontFamily:"motor" ),
+          ),
+        ),
+      ),
+    );
+
+  }
+  Padding buildListTileShave(Color glowColor,Color iconColor,String iconPath,String text) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Card(
+        elevation: 20,
+        child: ListTile(
+          onTap: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>ScorenNaviPage()));
+          },
+          trailing: AvatarGlow(
+            glowColor: glowColor,
+            endRadius: 20.0,
+            duration: Duration(milliseconds: 1000),
+            repeat: true,
+            showTwoGlows: true,
+            repeatPauseDuration: Duration(milliseconds: 300),
+            child: Icon(CupertinoIcons.antenna_radiowaves_left_right,color: iconColor,size: 30),
+          ),
+          contentPadding: EdgeInsets.fromLTRB(10,20,20,20,),
+          leading: Image.asset(iconPath),
+          title:Text(text,style: TextStyle(color: Colors.black54,fontSize: 20,fontFamily:"motor" ),
+          ),
+        ),
+      ),
+    );
+
   }
 }
--- lib/views/SettingsPage.dart
+++ lib/views/SettingsPage.dart
@@ -2,6 +2,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:app_settings/app_settings.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:rating_dialog/rating_dialog.dart';
+import 'package:url_launcher/url_launcher.dart';
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
 
@@ -9,11 +12,14 @@ class SettingsPage extends StatefulWidget {
   _SettingsPageState createState() => _SettingsPageState();
 }
 
+
 class _SettingsPageState extends State<SettingsPage> {
   final user = FirebaseAuth.instance.currentUser!;
+  final Uri _url = Uri.parse('https://github.com/erenkaraboga');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar:  AppBar(
         backgroundColor: Colors.white,
         title: Center(
@@ -25,108 +31,154 @@ class _SettingsPageState extends State<SettingsPage> {
           ),
         ),
       ),
-    body: Column(
-        children: [
-          Card(
-            elevation: 5,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(25.0),
-            ),
-            margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
-            shadowColor: Colors.grey,
-            child: InkWell(
-              borderRadius: BorderRadius.circular(25.0),
-              onTap: (){
-              },
-              child: ListTile(
-                  leading:  CircleAvatar(radius: (30),
-                      backgroundColor: Colors.white,
-                      child: ClipOval(
+    body: SingleChildScrollView(
+      child: Column(
+          children: [
+            Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25.0),
+              ),
+              margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+              shadowColor: Colors.grey,
+              child: InkWell(
+                borderRadius: BorderRadius.circular(25.0),
+                onTap: (){
+                },
+                child: ListTile(
+                    leading:  CircleAvatar(radius: (30),
+                        backgroundColor: Colors.white,
+                        child: ClipOval(
 
-                        child: Image.asset('assets/images/aygunlogo.jpg',fit: BoxFit.cover,),
-                      )
-                  ),
-                  subtitle: Text(user.email.toString()),
-                  title: Text("Admin")
+                          child: Image.asset('assets/images/aygunlogo.jpg',fit: BoxFit.cover,),
+                        )
+                    ),
+                    subtitle: Text(user.email.toString()),
+                    title: Text("Admin")
+                ),
               ),
             ),
-          ),
-          Card(
-            elevation: 5,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(25.0),
-            ),
-            margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
-            shadowColor: Colors.grey,
-            child: Column(
-              children: [
-                InkWell(child: buildListTile(Icon(CupertinoIcons.person),"Profil"),
-                  borderRadius: BorderRadius.circular(25.0),
-                  onTap: (){
-                  },),
-                InkWell(child: buildListTile(Icon(CupertinoIcons.lock),"Güvenlik"),
-                  borderRadius: BorderRadius.circular(25.0),
-                  onTap: (){
-                  },),
-                InkWell(child: buildListTile(Icon(CupertinoIcons.bell),"Bildirimler"),
-                  borderRadius: BorderRadius.circular(25.0),
-                  onTap: (){
-                  },),
-                InkWell(child: buildListTile(Icon(CupertinoIcons.bluetooth),"Bluetooth"),
-                  borderRadius: BorderRadius.circular(25.0),
-                  onTap: (){
-                    AppSettings.openBluetoothSettings();
-                  },) ,
-                ],
-            ),
-          ),
-          Card(
-            elevation: 5,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(25.0),
-            ),
-            margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
-            shadowColor: Colors.grey,
-            child: Column(
-              children: [
-                InkWell(child: buildListTile(Icon(Icons.lightbulb_outline),"Deneysel Özellikler"),
-                  borderRadius: BorderRadius.circular(25.0),
-                  onTap: (){
-                  },),
-              ],
-            ),
-          ),
-          Card(
-            elevation: 5,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(25.0),
-            ),
-            margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
-            shadowColor: Colors.grey,
-            child: Column(
-              children: [
-                InkWell(child: buildListTile(Icon(Icons.help_outline_outlined),"Yardım"),
-                  borderRadius: BorderRadius.circular(25.0),
-                  onTap: (){
-                  },),
-                InkWell(child: buildListTile(Icon(Icons.star_outline_outlined),"Değerlendir"),
+            Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25.0),
+              ),
+              margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+              shadowColor: Colors.grey,
+              child: Column(
+                children: [
+                  InkWell(child: buildListTile(Icon(CupertinoIcons.person),"Profil"),
                     borderRadius: BorderRadius.circular(25.0),
                     onTap: (){},
-                ),
-                InkWell(child: buildListTile(Icon(Icons.power_settings_new_outlined),"Çıkış yap"),
+
+                  ),
+                  InkWell(child: buildListTile(Icon(CupertinoIcons.lock),"Güvenlik"),
                     borderRadius: BorderRadius.circular(25.0),
                     onTap: (){
-                    FirebaseAuth.instance.signOut();
-                  },
-                ),
-              ],
+                    },),
+                  InkWell(child: buildListTile(Icon(CupertinoIcons.bell),"Bildirimler"),
+                    borderRadius: BorderRadius.circular(25.0),
+                    onTap: (){
+                    },),
+                  InkWell(child: buildListTile(Icon(CupertinoIcons.bluetooth),"Bluetooth"),
+                    borderRadius: BorderRadius.circular(25.0),
+                    onTap: (){
+                      AppSettings.openBluetoothSettings();
+                    },) ,
+                  ],
+              ),
             ),
-          ),
-        ],
+            Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25.0),
+              ),
+              margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+              shadowColor: Colors.grey,
+              child: Column(
+                children: [
+                  InkWell(child: buildListTileScoren(Icon(Icons.lightbulb_outline), "Deneysel Ürünler", "Scoren"),
+                    borderRadius: BorderRadius.circular(25.0),
+                    onTap: (){
+                    },),
+                ],
+              ),
+            ),
+            Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25.0),
+              ),
+              margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+              shadowColor: Colors.grey,
+              child: Column(
+                children: [
+                  InkWell(child: buildListTile(Icon(Icons.help_outline_outlined),"Yardım"),
+                    borderRadius: BorderRadius.circular(25.0),
+                    onTap: ()=>_launchUrl(),),
+                  InkWell(child: buildListTile(Icon(Icons.star_outline_outlined),"Değerlendir"),
+                      borderRadius: BorderRadius.circular(25.0),
+                      onTap: ()=>openDialog(),
+                  ),
+                  InkWell(child: buildListTile(Icon(Icons.power_settings_new_outlined),"Çıkış yap"),
+                      borderRadius: BorderRadius.circular(25.0),
+                      onTap: (){
+                      FirebaseAuth.instance.signOut();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+      ),
     )
     );
   }
+  Widget buildRatingg()=>RatingDialog(
+    initialRating: 1.0,
+    // your app's name?
+    title: Text(
+      'Puanla!',
+      textAlign: TextAlign.center,
+      style: const TextStyle(
+        fontSize: 25,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    // encourage your user to leave a high rating?
+    message: Text(
+      'Yıldıza tıkla ve puanla. İstersen yorum yapabilirsin!',
+      textAlign: TextAlign.center,
+      style: const TextStyle(fontSize: 15),
+    ),
+    // your app's logo?
+    image: Image.asset('assets/images/aygunlogo.jpg',fit: BoxFit.fitWidth,
+      width: 150,
+      height: 75,
+    ),
 
+    submitButtonText: 'Gönder',
+    submitButtonTextStyle: TextStyle(
+      color: Colors.grey.shade800,
+      fontSize: 20,
+      fontWeight:  FontWeight.bold
+    ),
+    commentHint: 'Yorumunuz...',
+    starColor: Colors.red
+    ,
+    onCancelled: () => print('cancelled'),
+    onSubmitted: (response) {
+      print('rating: ${response.rating}, comment: ${response.comment}');
+    },
+  );
+  void _launchUrl() async {
+    if (!await launchUrl(_url)) throw 'Could not launch $_url';
+  }
+  openDialog()=> showDialog(
+   context: context,
+   barrierDismissible: true, // set to false if you want to force a rating
+   builder: (context) => buildRatingg(),
+ );
   ListTile buildListTile(Icon icon ,String name) {
     return ListTile(
       trailing: Icon(CupertinoIcons.chevron_forward),
@@ -136,5 +188,16 @@ class _SettingsPageState extends State<SettingsPage> {
                  ),
                  title: Text(name),);
   }
+  ListTile buildListTileScoren(Icon icon ,String name,String subTitle) {
+    return ListTile(
+      subtitle: Text(subTitle,style: TextStyle(
+        fontWeight: FontWeight.bold
+      ),),
+      trailing: Icon(CupertinoIcons.chevron_forward),
+      leading:  Container(
+        height: double.infinity,
+        child: icon,
+      ),
+      title: Text(name),);
+  }
 }
-/*,*/
